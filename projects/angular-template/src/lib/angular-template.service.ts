61f400da6291ae159e1e7b96cda796e34b5c665f
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-09 18:30:28 +0300

    angular-template

---
+++ projects/angular-template/src/lib/angular-template.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { AngularTemplateModule } from './angular-template.module';
+
+@Injectable({
+  providedIn: AngularTemplateModule,
+})
+export class AngularTemplateService {
+
+  constructor() { }
+}
---
+++ projects/angular-template/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-template
+ */
+
+export * from './lib/angular-template.service';
+export * from './lib/angular-template.component';
+export * from './lib/angular-template.module';
