bc46696ddef8bccacc48bc52364e1be226b95a06
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-09 22:52:34 +0300

    settings added

--- projects/angular-template/src/lib/services/template.service.ts
+++ projects/angular-template/src/lib/services/template.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { TemplateSettings } from '../models/template-settings.model';
 
 @Injectable({
   providedIn: TemplateService,
 })
 export class TemplateService {
 
-  constructor() { }
+  public settings: TemplateSettings;
+
+  constructor(settings: TemplateSettings) {
+    this.settings = settings;
+  }
 }
--- projects/angular-template/src/public_api.ts
+++ projects/angular-template/src/public_api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of template
  */
 
+export * from './lib/models/template-settings.model';
 export * from './lib/services/template.service';
 export * from './lib/template.module';
