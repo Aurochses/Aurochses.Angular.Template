5e9d8d800071aee17f621da126cfa65821e4ab39
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-09 23:04:09 +0300

    solve settings in service and html

--- projects/angular-template/src/lib/services/template.service.ts
+++ projects/angular-template/src/lib/services/template.service.ts
@@ -11,4 +11,17 @@ export class TemplateService {
   constructor(settings: TemplateSettings) {
     this.settings = settings;
   }
+
+  toggleSidenav() {
+    this.settings.sidenav.open = !this.settings.sidenav.open;
+    this.resizeSidenav();
+  }
+
+  resizeSidenav() {
+    if (this.settings.sidenav.mode === 'side') {
+      const resizeEvent = document.createEvent('HTMLEvents');
+      resizeEvent.initEvent('resize', true, true);
+      document.dispatchEvent(resizeEvent);
+    }
+  }
 }
--- projects/angular-template/src/lib/toolbar/toolbar.component.ts
+++ projects/angular-template/src/lib/toolbar/toolbar.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
+import { TemplateService } from '../services/template.service';
+
 @Component({
   selector: 'aur-toolbar',
   templateUrl: './toolbar.component.html'
 })
-export class ToolbarComponent { }
+export class ToolbarComponent {
+
+  constructor(private templateService: TemplateService) { }
+
+}
