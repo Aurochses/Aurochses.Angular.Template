60dab1282d93b7d09d79a8327e6910b14906c2e0
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-09 19:30:40 +0300

    fix material package

--- projects/angular-template/src/lib/template.module.ts
+++ projects/angular-template/src/lib/template.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material';
+
 import { TemplateComponent } from './template.component';
 
 @NgModule({
   imports: [
+    MatToolbarModule
   ],
   declarations: [TemplateComponent],
   exports: [TemplateComponent]
