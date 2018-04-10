2e61db0e88e83c1e67f25373043f412cb9b71b5a
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-10 15:52:43 +0300

    menu works

--- projects/angular-template/src/lib/template.module.ts
+++ projects/angular-template/src/lib/template.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule } from '@angular/material';
 
 import { TemplateService } from './services/template.service';
 import { TemplateComponent } from './template.component';
@@ -9,17 +10,22 @@ import { TemplateComponent } from './template.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { LogoComponent } from './toolbar/logo/logo.component';
 
+import { SidenavComponent } from './sidenav/sidenav.component';
+
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule
   ],
   declarations: [
     TemplateComponent,
     ToolbarComponent,
-    LogoComponent
+    LogoComponent,
+    SidenavComponent
   ],
   providers: [
     TemplateService
