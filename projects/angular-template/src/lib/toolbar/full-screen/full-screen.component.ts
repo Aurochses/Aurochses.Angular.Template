57e8ec4b17613988c0dcb70861a29faca280673c
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-11 15:49:36 +0300

    full screen ready

--- projects/angular-template/src/lib/toolbar/full-screen/full-screen.component.ts
+++ projects/angular-template/src/lib/toolbar/full-screen/full-screen.component.ts
@@ -8,6 +8,34 @@ import { TemplateService } from '../../services/template.service';
 })
 export class FullScreenComponent {
 
-  constructor(private templateService: TemplateService) { }
+  toggleFullscreen() {
+    const fullscreenEnabled = document.fullscreenElement
+      || (<any>document).msFullscreenElement
+      || (<any>document).mozFullScreen
+      || document.webkitIsFullScreen;
+
+    if (!fullscreenEnabled) {
+      const func = (document.documentElement['requestFullscreen'])
+        || (document.documentElement['webkitRequestFullscreen'])
+        || (document.documentElement['webkitRequestFullScreen'])
+        || (document.documentElement['msRequestFullscreen'])
+        || (document.documentElement['mozRequestFullScreen']);
+
+      if (func) {
+        func.call(document.documentElement);
+      }
+
+    } else {
+      const func = (document['exitFullscreen'])
+        || (document['webkitExitFullscreen'])
+        || (document['webkitCancelFullScreen'])
+        || (document['msExitFullscreen'])
+        || (document['mozCancelFullScreen']);
+
+      if (func) {
+        func.call(document);
+      }
+    }
+  }
 
 }
