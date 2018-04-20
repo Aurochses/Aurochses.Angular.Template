93fb5879b9c102d4aefc8c80e50ae445ca379260
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-20 18:38:50 +0300

    added auth

--- projects/angular-template/src/lib/toolbar/user/user.component.ts
+++ projects/angular-template/src/lib/toolbar/user/user.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService, UserProfileModel } from '@aurochses/angular-auth';
 
 @Component({
   selector: 'aur-toolbar-user',
   templateUrl: './user.component.html',
   styleUrls: ['user.component.scss'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
 
-  constructor() { }
+  userProfile: UserProfileModel;
+
+  constructor(private router: Router, private authenticationService: AuthenticationService) { }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+
+    this.authenticationService.getUser().subscribe(user => {
+      console.log('getUser');
+
+      if (user) {
+        console.log(user);
+
+        this.userProfile = user.profile;
+      }
+    });
+  }
+
+  signIn(): void {
+    this.authenticationService.signInRedirect(this.router.url);
+  }
 
 }
