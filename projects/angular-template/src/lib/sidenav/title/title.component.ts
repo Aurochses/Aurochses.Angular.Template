d16f3eaf308c3a08d35eec119c1252f7b20cb44c
Vladimir Rodchenko <vladimir@aurochses.com>
2018-05-01 18:43:31 +0300

    title solved

--- projects/angular-template/src/lib/sidenav/title/title.component.ts
+++ projects/angular-template/src/lib/sidenav/title/title.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NavigationEnd, Route, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
 import { TranslateService } from '@ngx-translate/core';
 
-import { MenuItemModel } from '../../models/menu-item.model';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'aur-sidenav-title',
@@ -13,21 +13,11 @@ export class TitleComponent implements OnInit {
 
   title: string;
 
-  constructor(private router: Router, public translate: TranslateService) { }
+  constructor(public translateService: TranslateService, private menuService: MenuService) { }
 
   ngOnInit() {
-    this.title = this.getTitle(this.router.url);
-    this.router.events.subscribe((e) => {
-      if (e instanceof NavigationEnd) {
-        this.title = this.getTitle(e.url);
-      }
-    });
-  }
-
-  private getTitle(route) {
-    // TODO: need solution for tree menu cases
-    let obj = this.router.config.find((e) => e.path === route.substr(1));
-    return obj.data.title;
+    this.menuService.getActivatedRouteData()
+      .subscribe((routeData) => this.title = routeData.title);
   }
 
 }
