80d55258315aedae747f3bf0ab0223213fc9985f
Vladimir Rodchenko <vladimir@aurochses.com>
2018-05-10 16:35:10 +0300

    solve no applications and loading

--- projects/angular-template/src/lib/template.module.ts
+++ projects/angular-template/src/lib/template.module.ts
@@ -2,10 +2,14 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import {
-  MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule, MatListModule,
-  MatSelectModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -45,6 +49,7 @@ import { TitleComponent } from './sidenav/title/title.component';
     MatSidenavModule,
     MatListModule,
     MatSelectModule,
+    MatProgressSpinnerModule,
     FlexLayoutModule,
     TranslateModule,
     AuthenticationModule
