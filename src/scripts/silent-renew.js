e781aee18af5a496e7b04f34470472fe35164962
Vladimir Rodchenko <vladimir@aurochses.com>
2018-07-25 13:10:39 +0300

    update auth

--- src/scripts/silent-renew.js
+++
@@ -1,8 +0,0 @@
-var Oidc = window.Oidc;
-var UserManager = Oidc.UserManager;
-    
-if (Oidc && Oidc.Log && Oidc.Log.logger) {
-    Oidc.Log.logger = console;
-}
-
-new UserManager().signinSilentCallback();
